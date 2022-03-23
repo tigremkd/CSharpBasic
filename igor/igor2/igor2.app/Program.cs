c4f6bab91f669040bb1db7791cda264a1634a266
Igor Nikoloski <tigremkd@hotmail.com>
2022-03-23 13:01:28 -0700

    init

---
+++ igor/igor2/igor2.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace igor2.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
